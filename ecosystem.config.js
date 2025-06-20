b657f712c84c465d2eac6321ad021d436a39a706
FranOrteg <ortegfran@gmail.com>
2025-06-20 14:12:43 +0200

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,27 @@ module.exports = {
       env: {
         PYTHONUNBUFFERED: '1',
       }
+    },
+    {
+      name: 'report-gen',
+      script: '.venv/bin/python',
+      args: '-m src.report_scheduler',
+      cwd: '/home/ubuntu/quant-bot',
+      interpreter: 'none',
+      env: {
+        PYTHONUNBUFFERED: '1',
+      }
+    },
+    {
+      name: 'dashboard',
+      script: '.venv/bin/python',
+      args: '-m src.web_dashboard',
+      cwd: '/home/ubuntu/quant-bot',
+      interpreter: 'none',
+      env: {
+        FLASK_ENV: 'development',
+        PYTHONUNBUFFERED: '1',
+      }
     }
   ]
-};
+}
