b35bb553fcfcedead705e3c1206d382fd39a0f84
FranOrteg <ortegfran@gmail.com>
2025-07-30 14:15:41 +0200

    remove test files

--- src/test_api.py
+++
@@ -1,8 +0,0 @@
-# test_api.py
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-print("BINANCE_API_KEY:", os.getenv("BINANCE_API_KEY"))
-print("BINANCE_API_SECRET:", os.getenv("BINANCE_API_SECRET"))
